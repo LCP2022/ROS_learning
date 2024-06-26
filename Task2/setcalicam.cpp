0df05aae0ecd67eab221b27b17bc806ec2717e31
LCP2022 <97502231+LCP2022@users.noreply.github.com>
2024-06-26 13:45:13 +0800

    Update setcalicam.cpp
    Get Output.yaml paramete and display live pose estimation

--- Task2/setcalicam.cpp
+++ Task2/setcalicam.cpp
@@ -15,7 +15,9 @@ struct CameraCablparam{
     string Imagepath;
     int width,height;
     double fx,fy,cx,cy,k1,k2,p1,p2,k3,rms;
-    Mat cameraMatrix,distCoeffs,R,T;
+    Mat cameraMatrix,distCoeffs;
+    vector<Mat> R,T;
+    vector<vector<Point2f>> imgpoints; // Store 2D points
 };
 cv::Scalar BLUE = cv::Scalar(255,0,0);
 cv::Scalar GREEN = cv::Scalar(0,255,0);
@@ -53,36 +55,86 @@ CameraCablparam LoadCalibrateSetting(FileStorage fs){
     root["Distortion coefficients"]["distCoeffs"]>>ccparam.distCoeffs;
     root["Extrinsic-Camera-Parameter"]["Translation"]>>ccparam.T;
     root["Extrinsic-Camera-Parameter"]["Rotation"]>>ccparam.R;
+    //root["Image_points"]>>ccparam.imgpoints;
     return ccparam;
 }
 
-void pose_estimation(Mat in_image,CameraCablparam cali)
-{   float boxwidth = 1;
+void pose_estimation(Mat in_image,CameraCablparam cali,vector<Point2f> incorner_pts,  vector<Point3f> inobjp)
+{   
+    float boxwidth = 1;
     std::vector<cv::Point3f> cubePoints = {
         {0, 0, 0}, {0, boxwidth, 0}, {boxwidth, boxwidth, 0}, {boxwidth, 0, 0},
         {0, 0, -boxwidth}, {0, boxwidth, -boxwidth}, {boxwidth, boxwidth, -boxwidth}, {boxwidth, 0, -boxwidth}
     };
-    std::vector<cv::Point2f> imgptsss;
-    cv::projectPoints(cubePoints,cali.R,cali.T,cali.cameraMatrix,cali.distCoeffs,imgptsss);
     
+    std::vector<cv::Point2f> imgptsss;
+    Mat Rot = cv::Mat::zeros(1, 3, CV_32F);
+    Mat Tran = cv::Mat::zeros(1, 3, CV_32F);
+    bool ok =cv::solvePnP(inobjp,incorner_pts,cali.cameraMatrix,cali.distCoeffs,Rot,Tran,false,SOLVEPNP_ITERATIVE);
+    cv::projectPoints(cubePoints,Rot,Tran,cali.cameraMatrix,cali.distCoeffs,imgptsss);
+    Mat dummyimage;
+    in_image.copyTo(dummyimage);
    // Draw cube edges
-    cv::polylines(in_image, std::vector<std::vector<cv::Point>>{{imgptsss[0], imgptsss[1], imgptsss[2], imgptsss[3]}}, true,GREEN, 2); // front face
-    cv::polylines(in_image, std::vector<std::vector<cv::Point>>{{imgptsss[4], imgptsss[5], imgptsss[6], imgptsss[7]}}, true, BLUE, 2); // back face
+    cv::polylines(dummyimage, vector<vector<Point>>{{imgptsss[0], imgptsss[1], imgptsss[2], imgptsss[3]}}, true,GREEN, 2); // front face
+    cv::polylines(dummyimage, vector<vector<Point>>{{imgptsss[4], imgptsss[5], imgptsss[6], imgptsss[7]}}, true, BLUE, 2); // back face
     for (int i = 0; i < 4; ++i) {
-        cv::line(in_image, imgptsss[i], imgptsss[i + 4], RED, 2); // connecting lines
+        cv::line(dummyimage, imgptsss[i], imgptsss[i + 4], RED, 2); // connecting lines
     }
   
    // cv::line(in_image,YELLOW,4);
-    cv::imshow("pose estimation",in_image);
+    cv::imshow("pose estimation",dummyimage);
 }
 
 int main(int argc , char** argv){
     FileStorage fs;
-    bool status = LoadFile("/home/user/Desktop/CaliCam/src/piccali/config/Output.yaml",fs);
+    bool Outstatus = LoadFile("/home/user/Desktop/CaliCam/src/piccali/config/Output.yaml",fs);
     CameraCablparam listparam;
     listparam = LoadCalibrateSetting(fs);
-    Mat frame = imread(listparam.Imagepath);
-    fs.release();
+    // fs.release();
+
+    bool Inputstatus = LoadFile("/home/user/Desktop/CaliCam/src/piccali/config/InputSetting.xml",fs);
+    string devicepath;
+    int Chessboard_Height,Chessboard_Width;
+    fs["CHECKBOARD_WIDTH"]>>Chessboard_Width;
+    fs["CHECKBOARD_HEIGHT"]>>Chessboard_Height;
+    fs["deivce_path"] >> devicepath;
+
+    vector<Point3f> objp; // Defining the world coordinates for 3D points
+    for(int i{0}; i<Chessboard_Height; i++){
+        for(int j{0}; j<Chessboard_Width; j++)
+        objp.push_back(cv::Point3f(j,i,0));
+    }
+    VideoCapture cap(0);
+    if (!cap.isOpened()) {
+    cerr << "ERROR! Unable to open camera\n";
+    return -1;
+    }
+    Mat Frame,gray;
+    while(1){
+        cap.read(Frame);
+        if (Frame.empty()) {
+        cerr << "ERROR! blank frame grabbed\n";
+        break;
+        }
+        cvtColor(Frame,gray,cv::COLOR_BGR2GRAY);
+        std::vector<cv::Point2f> corner_pts; // Store  pixel coordinates of detected checker board corners 
+        bool Found = cv::findChessboardCorners(gray, cv::Size(Chessboard_Width,Chessboard_Height), corner_pts, CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FAST_CHECK | CV_CALIB_CB_NORMALIZE_IMAGE);
+        if(Found){
+            
+            cv::TermCriteria criteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.001);
+            // refining pixel coordinates for given 2d points.
+            cv::cornerSubPix(gray,corner_pts,cv::Size(11,11), cv::Size(-1,-1),criteria);
+            pose_estimation(Frame,listparam,corner_pts,objp);
+        }
+        imshow("LiveView",Frame);
+        int value = waitKeyEx(30);
+        switch(value){
+            case 27 : return 0;break;
+        }
+    }
+    
+
+    
     return 0;
 }
 
